a49b03e41ae3b458641e9cae5ccdb64dc06d54f9
Ekaterina Zolotarevskaia <zkatya123@gmail.com>
2020-10-05 13:12:14 +0200

    Added solver_options to BAC_Problem

--- prototype/complete.jl
+++ prototype/complete.jl
@@ -27,46 +27,46 @@ begin #BAC.jl
         y0_sys
         solver
     end
-      
+
     function (bl::BAC_Problem)(p; solver_options...)
           # Evalute the loss function of the BAC problem
-      
+
           @views begin
               p_sys = p[1:bl.dim_sys]
               p_specs = [p[bl.dim_sys + 1 + (n - 1) * bl.dim_spec:bl.dim_sys + n * bl.dim_spec] for n in 1:bl.N_samples]
           end
-      
+
           loss = 0.
-      
+
           # For plotting evaluate the first sample outside the loop:
           n = 1
           i = bl.input_sample[n]
           dd_sys = solve(ODEProblem((dy,y,p,t) -> bl.f_sys(dy, y, i(t), p, t), bl.y0_sys, bl.t_span, p_sys), bl.solver; saveat=bl.tsteps, solver_options...)
           dd_spec = solve(ODEProblem((dy,y,p,t) -> bl.f_spec(dy, y, i(t), p, t), bl.y0_spec, bl.t_span, p_specs[n]), bl.solver; saveat=bl.tsteps, solver_options...)
           loss += bl.output_metric(dd_sys, dd_spec)
-      
+
           for n in 2:bl.N_samples
               i = bl.input_sample[n]
               dd_sys = solve(ODEProblem((dy,y,p,t) -> bl.f_sys(dy, y, i(t), p, t), bl.y0_sys, bl.t_span, p_sys), bl.solver; saveat=bl.tsteps, solver_options...)
               dd_spec = solve(ODEProblem((dy,y,p,t) -> bl.f_spec(dy, y, i(t), p, t), bl.y0_spec, bl.t_span, p_specs[n]), bl.solver; saveat=bl.tsteps, solver_options...)
               loss += bl.output_metric(dd_sys, dd_spec)
           end
-      
+
           loss, dd_sys, dd_spec
       end
-      
-      
-      function (bl::BAC_Problem)(n, p_sys, p_spec)
+
+
+      function (bl::BAC_Problem)(n, p_sys, p_spec;solver_options...)
           i = bl.input_sample[n]
           dd_sys = solve(ODEProblem((dy,y,p,t) -> bl.f_sys(dy, y, i(t), p, t), bl.y0_sys, bl.t_span, p_sys), bl.solver; saveat=bl.tsteps, solver_options...)
           dd_spec = solve(ODEProblem((dy,y,p,t) -> bl.f_spec(dy, y, i(t), p, t), bl.y0_spec, bl.t_span, p_spec), bl.solver; saveat=bl.tsteps, solver_options...)
-          
+
           loss = bl.output_metric(dd_sys, dd_spec)
-      
+
           loss, dd_sys, dd_spec
       end
-      
-      function bac_spec_only(bl::BAC_Problem, p_initial)
+
+      function bac_spec_only(bl::BAC_Problem, p_initial; solver_options...)
           # Optimize the specs only
           println("new2")
 
@@ -75,8 +75,8 @@ begin #BAC.jl
               p_sys = p[1:bl.dim_sys]
               p_specs = [p[bl.dim_sys + 1 + (n - 1) * bl.dim_spec:bl.dim_sys + n * bl.dim_spec] for n in 1:bl.N_samples]
           end
-    
-      
+
+
           for n in 1:bl.N_samples
             println(n)
             res = DiffEqFlux.sciml_train(
@@ -86,10 +86,10 @@ begin #BAC.jl
                 maxiters = 100)
               p_specs[n] .= res.minimizer
           end
-      
+
           p
       end
-      
+
       function individual_loss(bl::BAC_Problem, p_sys, p_specs, n; solver_options...)
           # Evalute the individual loss contributed by the nth sample
           i = bl.input_sample[n]
@@ -97,20 +97,20 @@ begin #BAC.jl
           dd_spec = solve(ODEProblem((dy,y,p,t) -> bl.f_spec(dy, y, i(t), p, t), bl.y0_spec, bl.t_span, p_specs[n]), bl.solver; saveat=bl.tsteps, solver_options...)
           bl.output_metric(dd_sys, dd_spec)
       end
-      
+
       function individual_losses(bl::BAC_Problem, p)
           # Return the array of losses
           @views begin
               p_sys = p[1:bl.dim_sys]
               p_specs = [p[bl.dim_sys + 1 + (n - 1) * bl.dim_spec:bl.dim_sys + n * bl.dim_spec] for n in 1:bl.N_samples]
           end
-      
+
           [individual_loss(bl::BAC_Problem, p_sys, p_specs, n) for n in 1:bl.N_samples]
       end
-      
-      
+
+
       # The constructors for BAC_Problem
-      
+
       function BAC_Problem(
           f_spec,
           f_sys,
@@ -123,10 +123,8 @@ begin #BAC.jl
           dim_sys::Int,
           y0_spec,
           y0_sys; solver = Tsit5())
-      
           BAC_Problem(f_spec, f_sys, tsteps, t_span, input_sample, output_metric, N_samples, dim_spec, dim_sys, y0_spec, y0_sys, solver)
       end
-      
       function BAC_Problem(
           f_spec,
           f_sys,
@@ -141,17 +139,13 @@ begin #BAC.jl
           y0_sys = zeros(dim_sys)
           BAC_Problem(f_spec, f_sys, tsteps, t_span, input_sample, output_metric, N_samples, dim_spec, dim_sys, y0_spec, y0_sys, solver)
        end
-      
       # Resampling the BAC_Problem
       export resample
       function resample(sampler, bac::BAC_Problem)
           new_input_sample = [sampler(n) for n in 1:bac.N_samples]
           BAC_Problem(bac.f_spec, bac.f_sys, bac.tsteps, bac.t_span, new_input_sample, bac.output_metric, bac.N_samples, bac.dim_spec, bac.dim_sys, bac.y0_spec, bac.y0_sys, bac.solver)
       end
-      
       # Basic callbacks
-      
-      
       function basic_bac_callback(p, loss, dd_sys, dd_spec)
           display(loss)
           # Tell sciml_train to not halt the optimization. If return true, then
@@ -263,7 +257,7 @@ begin # PlotUtils
         end
         end
     end
-    
+
 end # module
 
 Random.seed!(42);
@@ -350,3 +344,7 @@ for i in 1:30
     l, sol1, sol2 = bac_100(res_100.minimizer);
     plot_callback(res_100.minimizer, l, sol1, sol2)
 end
+
+losses_100 = individual_losses(bac_100, res_100.minimizer)
+# Todo: Indication of bug or not completely understood behaviour!!
+median(losses_100)
