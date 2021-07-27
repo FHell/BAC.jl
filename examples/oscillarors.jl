83d05139eea686113977f6fb5d06c47c7e7c7bd0
Ekaterina Zolotarevskaia <zkatya123@gmail.com>
2021-07-27 22:42:57 +0200

    Change parametrization from frequency to coupling

--- examples/oscillarors.jl
+++ examples/oscillarors.jl
@@ -10,21 +10,23 @@ using LightGraphs
 using Statistics
 using DataFrames, Pipe
 using LaTeXStrings
+using LinearAlgebra
 
 include("src/Core.jl")
 include("src/ExampleSystems.jl")
 include("src/PlotUtils.jl")
 include("src/Benchmark.jl")
 
-mutable struct kuramoto_osc{K, N, theta} #mutable for the development
-    K::K
+mutable struct kuramoto_osc{w, N, theta}
+    #K::K
+    w::w
     N::N
     theta::theta
 end
 
 function (dd::kuramoto_osc)(dx, x, i, p, t)
     # x -> Theta, p -> omega
-    dx .= [p[k] + dd.K*sum(sin(x[k]-x[j]) for j in 1:dd.N)/dd.N for k in 1:dd.N]
+    dx .= [dd.w[k] + sum(p[k,j]*sin(x[k]-x[j]) for j in 1:dd.N)/dd.N for k in 1:dd.N]
     dx[1] += i
     # the relu(p) should be handles outside the function definition
     nothing
@@ -35,11 +37,11 @@ mutable struct KuramotoOutputMetric
     n_spec
 end
 
-function create_kuramoto_example(K,dim_sys, dim_spec, tsteps, N_samples; modes = 5)
+function create_kuramoto_example(w, dim_sys, dim_spec, tsteps, N_samples; modes = 5)
     #K_sys = rand(dim_sys) - if K was an optimization parameter
     # scale down the input by a factor of 0.1
-    f_spec = kuramoto_osc(K, dim_spec, zeros(dim_spec))
-    f_sys = kuramoto_osc(K, dim_sys, zeros(dim_sys))
+    f_spec = kuramoto_osc(w[dim_sys+1:end], dim_spec, zeros(dim_spec))
+    f_sys = kuramoto_osc(w[1:dim_sys], dim_sys, zeros(dim_sys))
     BAC_Loss(
         f_spec,
         f_sys,
@@ -70,9 +72,11 @@ end
 dim_sys = 10
 dim_spec = 2
 N_samples = 10
-kur = create_kuramoto_example(0.1,dim_sys,dim_spec,0.:100.,N_samples)
-
-p_initial = ones(dim_sys + dim_spec * N_samples);
+omega = ones(dim_sys+dim_spec)
+kur = create_kuramoto_example(omega,dim_sys,dim_spec,0.:100.,N_samples)
+K_sys_init = 3*ones(dim_sys,dim_sys)
+K_spec_init = 3*ones(dim_spec,dim_spec)
+p_initial = [K_sys_init zeros(dim_sys, dim_spec);zeros(dim_spec,dim_sys) K_spec_init]
 
 kur(p_initial)
 #plot(kur.tsteps, kur.input_sample, c=:gray, alpha=1, legend=false)
@@ -88,5 +92,3 @@ res_10 = DiffEqFlux.sciml_train(
     cb = basic_bac_callback
     #cb = (p, l) -> plot_callback(kur, p, l, scenario_nums=scenarios)
     )
-
-i =  rand_fourier_input_generator()
