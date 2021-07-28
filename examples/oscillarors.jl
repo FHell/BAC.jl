97494971fd68705c5679a7af50680a8018f41840
Ekaterina Zolotarevskaia <zkatya123@gmail.com>
2021-07-28 18:08:51 +0200

    create symmetrical coupling example

--- examples/oscillarors.jl
+++ examples/oscillarors.jl
@@ -17,37 +17,74 @@ include("src/ExampleSystems.jl")
 include("src/PlotUtils.jl")
 include("src/Benchmark.jl")
 
-mutable struct kuramoto_osc{w, N, theta}
-    #K::K
+mutable struct kuramoto_osc{w, N, theta, K}
     w::w
     N::N
     theta::theta
+    K_av::K
 end
+
+
 
 function (dd::kuramoto_osc)(dx, x, i, p, t)
     # x -> Theta, p -> omega
-    dx .= [dd.w[k] + sum(p[k,j]*sin(x[k]-x[j]) for j in 1:dd.N)/dd.N for k in 1:dd.N]
+    p_total = sum(abs.(p))
+    #dx .= [dd.w[k] + sum(p[k,j]/p_total*sin(x[k]-x[j]) for j in 1:dd.N)/dd.N for k in 1:dd.N]
+    for k in 1:dd.N
+        dx[k] = 0.
+        for j in 1:k
+            dx[k]-=abs(p[k,j])*sin(x[k]-x[j])
+            dx[j]+=abs(p[k,j])*sin(x[k]-x[j])
+        end
+        dx[k] *= dd.K_av/p_total
+        dx[k] += dd.w[k]
+    end
     dx[1] += i
     # the relu(p) should be handles outside the function definition
     nothing
 end
 
+##
+
+dim_sys = 10
+dim_spec = 2
+N_samples = 10
+K_sys_init = 3*rand(dim_sys,dim_sys).+1.
+
+i = rand_fourier_input_generator(1)
+##
+omega = 3*rand(dim_sys)
+omega .-= mean(omega)
+kur_ex = kuramoto_osc(omega, dim_sys, zeros(dim_sys), 50.)
+
+dd_sys = solve(ODEProblem((dy,y,p,t) -> kur_ex(dy, y, i(t), p, t), ones(dim_sys), (0.,100.),  K_sys_init), Tsit5())
+#=dy0 = zeros(dim_sys)
+y0 = ones(dim_sys)
+kur_ex(dy0, y0, i(0.),K_sys_init, 0.)=#
+plot(dd_sys)
+#=
+solve_sys_spec(kur_ex, rand_fourier_input_generator(1), p_initial[1:dim_sys, 1:dim_sys], p_initial[dim_sys:end, dim_sys:end])
+=#
+##
+
+
+##
 mutable struct KuramotoOutputMetric
     n_sys
     n_spec
 end
 
-function create_kuramoto_example(w, dim_sys, dim_spec, tsteps, N_samples; modes = 5)
+function create_kuramoto_example(w, dim_sys, dim_spec, K,  tsteps, N_samples; modes = 5)
     #K_sys = rand(dim_sys) - if K was an optimization parameter
     # scale down the input by a factor of 0.1
-    f_spec = kuramoto_osc(w[dim_sys+1:end], dim_spec, zeros(dim_spec))
-    f_sys = kuramoto_osc(w[1:dim_sys], dim_sys, zeros(dim_sys))
+    f_spec = kuramoto_osc(w[dim_sys+1:end], dim_spec, zeros(dim_spec), K)
+    f_sys = kuramoto_osc(w[1:dim_sys], dim_sys, zeros(dim_sys), K)
     BAC_Loss(
         f_spec,
         f_sys,
         tsteps,
         (tsteps[1], tsteps[end]),
-        [rand_fourier_input_generator(modes) for n = 1:N_samples], # input function i(t) 
+        [rand_fourier_input_generator(n, N = modes) for n = 1:N_samples], # input function i(t) 
         StandardOutputMetric(1, 1), # phase at interface node
         N_samples,
         dim_spec,
@@ -106,10 +143,10 @@ dim_sys = 10
 dim_spec = 2
 N_samples = 10
 omega = ones(dim_sys+dim_spec)
-kur = create_kuramoto_example(omega,dim_sys,dim_spec,0.:100.,N_samples)
+kur = create_kuramoto_example(omega,dim_sys,dim_spec,0.:100.,N_samples, 5.)
 K_sys_init = 3*ones(dim_sys,dim_sys)
 K_spec_init = 3*ones(dim_spec,dim_spec)
-#p_initial = [K_sys_init zeros(dim_sys, N_samples*dim_spec); zeros(N_samples*dim_spec, dim_sys+N_samples*dim_spec)]
+p_initial = [K_sys_init zeros(dim_sys, N_samples*dim_spec); zeros(N_samples*dim_spec, dim_sys+N_samples*dim_spec)]
 
 for i in 1:N_samples
     p_initial[(dim_sys+dim_spec*i-1):(dim_sys+dim_spec*i), (dim_sys+dim_spec*i-1):(dim_sys+dim_spec*i)] = K_spec_init
@@ -117,7 +154,7 @@ end
 
 kur(p_initial, dim = 2)
 #plot(kur.tsteps, kur.input_sample, c=:gray, alpha=1, legend=false)
-
+kur.f_sys(K_sys_init)
 
 solve_sys_spec(kur, rand_fourier_input_generator(1), p_initial[1:dim_sys, 1:dim_sys], p_initial[dim_sys:end, dim_sys:end])
 
@@ -130,3 +167,13 @@ res_10 = DiffEqFlux.sciml_train(
     #cb = (p, l) -> plot_callback(kur, p, l, scenario_nums=scenarios)
     )
 
+#=
+p = zeros(6*6 + 2*2)
+p_1 = view(p, 1:6*6)
+p_2 = view(p, 6*6 + 1:6*6 + 2*2)
+##
+
+p_2d_1 = reshape(p_1, (6,6))
+p_2d_2 = reshape(p_2, (2,2))
+
+=# 
